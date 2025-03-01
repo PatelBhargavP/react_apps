87df6c6176a6cdf0d71a3cf8fa3cb7966aa0d62a
PatelBhargavP <37446238+PatelBhargavP@users.noreply.github.com>
2025-03-01 18:26:46 +0530

    Styling component

--- first-react/src/main.jsx
+++ first-react/src/main.jsx
@@ -3,8 +3,67 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/**
+ * Challenge:
+ * 
+ * - Add a `nav` > `ul` > `li` (x3). The 3 items should say:
+ *   "Pricing", "About", and "Contact"
+ */
+
+function Header() {
+    return (
+        <header>
+            <img src="src/react-logo.svg" className='header-image' alt="React logo" />
+            <nav>
+              <ul className='nav-list'>
+                <li>
+                  Pricing
+                </li>
+                <li>
+                  About
+                </li>
+                <li>
+                  Contract
+                </li>
+              </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Page />
   </StrictMode>,
 )
