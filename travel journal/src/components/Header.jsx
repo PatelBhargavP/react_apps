a903fa4ca38cebc7ae2c72de1bc8615c2b93ff8e
PatelBhargavP <37446238+PatelBhargavP@users.noreply.github.com>
2025-04-26 17:28:17 +0530

    Added header componennt to the travel journal

---
+++ travel journal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header-element">
+            <img src="src/assets/globe.png" alt="globe"></img>
+            <h4>My travel journal</h4>
+        </header>
+    )
+}
