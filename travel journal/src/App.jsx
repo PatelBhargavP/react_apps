ac5a126dadd0cf0f5df17f406c4e0babb3e53712
PatelBhargavP <37446238+PatelBhargavP@users.noreply.github.com>
2025-04-27 12:20:27 +0530

    Added data through props

--- travel journal/src/App.jsx
+++ travel journal/src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Entry from './components/Entry'
+import data from './data';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  let entries = data.map(entry => {
+    return (
+      <Entry
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+        image={entry.img}
+      />
+    );
+  })
 
   return (
     <>
       <Header />
       <main className="container">
-        <Entry />
+        {entries}
       </main>
     </>
   )
